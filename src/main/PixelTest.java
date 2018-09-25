532eb98a4c724f852ec06127ce2298f17a55d711
Horák David 2 <horakda2@uhk.cz>
2018-09-25 17:31:42 +0200

    Initial commit

---
+++ src/main/PixelTest.java
@@ -0,0 +1,51 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+
+public class PixelTest {
+
+    private JFrame window;
+    private BufferedImage img;
+    private JPanel panel;
+
+
+    public PixelTest() {
+    window = new JFrame();
+    window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    window.setSize(800 , 600);
+
+    panel = new JPanel();
+    window.add(panel);
+    img = new BufferedImage(800,600, BufferedImage.TYPE_INT_RGB);
+
+    window.setVisible(true);
+    DrawPixel(100, 50, Color.GREEN.getRGB());
+
+    panel.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            DrawPixel(e.getX(), e.getY(), Color.GREEN.getRGB());
+        }
+    });
+
+
+
+    }
+
+    private void DrawPixel(int x,int y,int color){
+        img.setRGB(x,y, color);
+        panel.getGraphics().drawImage(img, 0, 0, null);
+
+}
+
+    public static void main(String[] args) {
+        new PixelTest();
+    }
+
+}
